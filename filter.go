1e01aec77700f1bfd73e078a2cef1ff1077671e0
Jared Watts <jbw976@gmail.com>
2018-11-02 23:03:31 -0700

    Add date field to filters
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- filter.go
+++ filter.go
@@ -7,16 +7,21 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
-const (
-	FilterFileName = `filter.json`
-)
+type JSONDate time.Time
 
 type Filter struct {
-	FilterRegex string `json:"regex"`
-	MinAmount   int    `json:"min"` // amount in cents, can be negative or positive
-	MaxAmount   int    `json:"max"` // amount in cents, can be negative or positive
+	FilterRegex string   `json:"regex"`
+	MinAmount   int      `json:"min"` // amount in cents, can be negative or positive
+	MaxAmount   int      `json:"max"` // amount in cents, can be negative or positive
+	Date        JSONDate `json:"date,omitempty"`
+}
+
+func (f Filter) String() string {
+	return fmt.Sprintf("[filter:'%s', min:%s, max:%s]", f.FilterRegex, printAmount(f.MinAmount), printAmount(f.MaxAmount))
 }
 
 func loadFilters(filterPath string) ([]Filter, error) {
@@ -46,12 +51,34 @@ func loadFilters(filterPath string) ([]Filter, error) {
 }
 
 func isFiltered(t *Transaction, filters []Filter) bool {
-	for i := range filters {
-		match, _ := regexp.MatchString("(?i)"+filters[i].FilterRegex, t.Description)
-		if match && t.Amount >= filters[i].MinAmount && t.Amount <= filters[i].MaxAmount {
-			return true
+	for _, f := range filters {
+		match, _ := regexp.MatchString("(?i)"+f.FilterRegex, t.Description)
+		if match && t.Amount >= f.MinAmount && t.Amount <= f.MaxAmount {
+			// the regex matches and the amount matches, let's also factor in the date if the filter has one
+			if time.Time(f.Date).IsZero() {
+				// date isn't specified on the filter, don't factor it in and just call this a match
+				return true
+			}
+
+			// check the filter's date to further verify this is a match
+			return t.Timestamp.Before(time.Time(f.Date).AddDate(0, 0, 1)) &&
+				t.Timestamp.After(time.Time(f.Date).AddDate(0, 0, -1))
 		}
 	}
 
 	return false
+}
+
+func (j *JSONDate) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	t, err := time.Parse("2006-01-02", s)
+	if err != nil {
+		return err
+	}
+	*j = JSONDate(t)
+	return nil
+}
+
+func (j *JSONDate) MarshalJSON() ([]byte, error) {
+	return json.Marshal(j)
 }
