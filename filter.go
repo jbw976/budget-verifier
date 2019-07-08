fbff5a0302babc3d0b115f85c222827c0f6cec58
Jared Watts <jbw976@gmail.com>
2019-07-07 19:46:17 -0700

    Clean up filter logic to allow for multiple filters that match description and amount, but could vary by date
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- filter.go
+++ filter.go
@@ -21,7 +21,12 @@ type Filter struct {
 }
 
 func (f Filter) String() string {
-	return fmt.Sprintf("[filter:'%s', min:%s, max:%s]", f.FilterRegex, printAmount(f.MinAmount), printAmount(f.MaxAmount))
+	var filterDate string
+	if !time.Time(f.Date).IsZero() {
+		filterDate = time.Time(f.Date).Format("2006-01-02")
+	}
+
+	return fmt.Sprintf("[filter:'%s', min:%s, max:%s, date:'%s']", f.FilterRegex, printAmount(f.MinAmount), printAmount(f.MaxAmount), filterDate)
 }
 
 func loadFilters(filterPath string) ([]Filter, error) {
@@ -53,16 +58,26 @@ func loadFilters(filterPath string) ([]Filter, error) {
 func isFiltered(t *Transaction, filters []Filter) bool {
 	for _, f := range filters {
 		match, _ := regexp.MatchString("(?i)"+f.FilterRegex, t.Description)
-		if match && t.Amount >= f.MinAmount && t.Amount <= f.MaxAmount {
-			// the regex matches and the amount matches, let's also factor in the date if the filter has one
-			if time.Time(f.Date).IsZero() {
-				// date isn't specified on the filter, don't factor it in and just call this a match
-				return true
-			}
+		if !match {
+			// description doesn't match filter regex, continue to next filter
+			continue
+		}
 
-			// check the filter's date to further verify this is a match
-			return t.Timestamp.Before(time.Time(f.Date).AddDate(0, 0, 1)) &&
-				t.Timestamp.After(time.Time(f.Date).AddDate(0, 0, -1))
+		if t.Amount > f.MaxAmount || t.Amount < f.MinAmount {
+			// amount doesn't match filter range, continue to next filter
+			continue
+		}
+
+		// the regex matches and the amount matches, let's also factor in the date if the filter has one
+		if time.Time(f.Date).IsZero() {
+			// date isn't specified on the filter, don't factor it in and just call this a match
+			return true
+		}
+
+		// check the filter's date to further verify this is a match
+		if t.Timestamp.Before(time.Time(f.Date).AddDate(0, 0, 1)) &&
+			t.Timestamp.After(time.Time(f.Date).AddDate(0, 0, -1)) {
+			return true
 		}
 	}
 
