552baf935fb363f59ba518fdb76fc889fff67125
Jared Watts <jbw976@gmail.com>
2020-09-08 19:01:41 -0700

    Add support for Chase credit card statement format
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- parse.go
+++ parse.go
@@ -23,13 +23,19 @@ func parseBankTransactions(bankRecords [][]string) ([]Transaction, error) {
 	var pi parseIndices
 	for i, record := range bankRecords {
 		if len(record) > 3 && record[0] == "Date" && record[1] == "Description" && record[2] == "Amount" {
+			// Bank of America debit format
 			// found the headers that precedes the useful records.  there's still 1 more useless record,
 			// so the good starting point is actually 1 greater than just the next index.
 			start = i + 2
 			pi = parseIndices{timestamp: 0, description: 1, amount: 2, details: -1}
 		} else if len(record) > 3 && record[0] == "Posted Date" && record[1] == "Reference Number" && record[2] == "Payee" {
+			// Bank of America credit format
 			start = i + 1
 			pi = parseIndices{timestamp: 0, description: 2, amount: 4, details: -1}
+		} else if len(record) > 4 && record[0] == "Transaction Date" && record[1] == "Post Date" && record[3] == "Category" {
+			// Chase credit format
+			start = i + 1
+			pi = parseIndices{timestamp: 0, description: 2, amount: 5, details: -1}
 		}
 	}
 
